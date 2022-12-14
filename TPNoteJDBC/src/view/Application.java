4614ebf6d95fded3a81e9ac887c8742a06d3ad48
Aurel <aurelien.dafonseca@gmail.com>
2022-12-14 16:10:43 +0100

    question 2

--- TPNoteJDBC/src/view/Application.java
+++ TPNoteJDBC/src/view/Application.java
@@ -5,7 +5,10 @@ import java.util.List;
 import java.util.Scanner;
 
 import controller.GestionAppreciation;
+import controller.GestionFilm;
 import model.Appreciation;
+import model.Film;
+import model.Projection;
 import utile.Bdd;
 
 public class Application {
@@ -25,14 +28,14 @@ public class Application {
 			showAppreciation(gestionAvis.afficherAvisFilm(nomSpectateur), nomSpectateur);
 			break;
 		}
-//		case 2: {
-//			GestionLivre controllerLivre = new GestionLivre();
-//			System.out.println("Spectateur: ");
-//			scanner = new Scanner(System.in);
-//			String nomSalle = scanner.nextLine();
-//			System.out.println(controllerLivre.ajouterExemplaireLivre(nomSalle));
-//			break;
-//		}
+		case 2: {
+			GestionFilm gestionFilm=new GestionFilm();
+			System.out.println("Salle: ");
+			scanner = new Scanner(System.in);
+			String nomSalle = scanner.nextLine();
+			showFilms(gestionFilm.afficherListeProjections(nomSalle),nomSalle);
+			break;
+		}
 		default:
 			throw new IllegalArgumentException("Unexpected value: " + choix);
 		}
@@ -41,7 +44,7 @@ public class Application {
 	private static void showMenu() {
 		System.out.println("choissiez");
 		System.out.println("1. Affichage des avis sur les films vu par un spectateur");
-		System.out.println("2. Affichage liste films projeté pour une salle ");
+		System.out.println("2. Affichage liste films projete pour une salle ");
 		System.out.println("3. Afficher les associes en retard dans leurs prets");
 	}
 	
@@ -53,6 +56,18 @@ public class Application {
 			System.out.println("Listes des Avis par film pour "+nomSpectateur);
 			for(Appreciation appreciation : listeAppreciation) {
 				System.out.println(appreciation.getFilm().getTitre()  +": "+appreciation.getNote());
+			}
+		}	
+	}
+	
+	private static void showFilms(List<Projection> listeProjections,String nomSalle) {
+		if(listeProjections.size()<=0) {
+			System.out.println("Aucun film prevu aujourd'hui dans cette salle.");
+		}
+		else {
+			System.out.println("Les films prevu aujourd hui dans la salle "+nomSalle +" sont :");
+			for(Projection projection:listeProjections){
+				System.out.println(projection.getFilm().getTitre()+" a "+projection.getHeure());
 			}
 		}
 	}
